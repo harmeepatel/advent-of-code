32f0382a5e0c8878124f82c65f50f525344ddec4
Harmy1410 <harmy1410@gmail.com>
2022-12-01 23:50:33 -0500

    complete

--- 22/day1/s.rs
+++ 22/day1/s.rs
@@ -6,6 +6,8 @@ use std::{
 fn main() {
     let f = File::open("input.txt").unwrap();
     let f = BufReader::new(f);
+
+    // a
     let mut cals: Vec<i32> = Vec::new();
     let mut cal = 0;
     for line in f.lines() {
@@ -20,10 +22,24 @@ fn main() {
         // println!("{}", line.unwrap().parse::<usize>().unwrap());
     }
     let mut max = 0;
-    for cal in cals {
-        if cal > max {
-            max = cal
+    for cal in cals.iter() {
+        if *cal > max {
+            max = *cal
         }
     }
     dbg!(max);
+
+    // b
+    let mut total = 0;
+    for _ in 0..3 {
+        let mut max = 0;
+        for cal in cals.iter() {
+            if *cal > max {
+                max = *cal
+            }
+        }
+        total = total + max;
+        cals.remove(cals.iter().position(|&x| x == max).unwrap());
+    }
+    dbg!(total);
 }
